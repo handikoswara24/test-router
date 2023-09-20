556e9e0684535b50a305ca8b2e8693ad310a16dd
handi.koswara <handi.koswara@technosoftautomotive.com>
2023-09-20 22:00:46 +0700

    change to class

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import './App.css';
 import { Route } from "react-router";
-import React from "react"
+import React, { Component } from "react"
 import Home from './Home';
 import Ask from "./Ask";
 import About from "./About"
 
-function App() {
+export default class App extends Component {
   // const baseUrl = document.getElementsByTagName('base')[0]?.getAttribute('href');
   // console.log(baseUrl);
-  return (
-    <div className="App">
-      <h1>Hiii</h1>
+  render() {
+    return (
+      <div className="App">
+        <h1>Hiii</h1>
 
-      <Route exact path="/" component={Home}></Route>
+        <Route exact path="/" component={Home}></Route>
 
-      <Route exact path="/ask" component={Ask}></Route>
+        <Route exact path="/ask" component={Ask}></Route>
 
-      <Route exact path="/about" component={About}></Route>
+        <Route exact path="/about" component={About}></Route>
 
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
-export default App;
+
