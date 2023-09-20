5c55ce181238f0c6eea59de7fffc9e921a7b711f
handi.koswara <handi.koswara@technosoftautomotive.com>
2023-09-20 21:51:52 +0700

    service worker

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
+import registerServiceWorker from './registerServiceWorker';
 
 const root = document.getElementById('root');
 // root.render(
@@ -18,5 +19,5 @@ ReactDOM.render(<Router basename={""}>
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-
+// reportWebVitals();
+registerServiceWorker()
