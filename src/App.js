0ca5a056456f3f1e347c9a85d1bfecd64c98cf14
handi.koswara <handi.koswara@technosoftautomotive.com>
2023-09-20 20:46:01 +0700

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route } from "react-router-dom";
+import { Route } from "react-router";
 import Home from './Home';
 import Ask from "./Ask";
 import About from "./About"
@@ -11,13 +10,13 @@ function App() {
   return (
     <div className="App">
       <h1>Hiii</h1>
-      <BrowserRouter basename={""}>
-        <Route exact path="/" component={Home}></Route>
 
-        <Route exact path="/ask" component={Ask}></Route>
+      <Route exact path="/" component={Home}></Route>
 
-        <Route exact path="/about" component={About}></Route>
-      </BrowserRouter>
+      <Route exact path="/ask" component={Ask}></Route>
+
+      <Route exact path="/about" component={About}></Route>
+
     </div>
   );
 }
