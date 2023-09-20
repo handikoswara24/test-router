51297227bc9799593f381ab914a021489ac9ffb1
handi.koswara <handi.koswara@technosoftautomotive.com>
2023-09-20 20:15:24 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from './Home';
+import Ask from "./Ask";
+import About from "./About"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hiii</h1>
+      <BrowserRouter>
+        <Route exact path="/" component={Home}></Route>
+
+        <Route exact path="/ask" component={Ask}></Route>
+
+        <Route exact path="/about" component={About}></Route>
+      </BrowserRouter>
     </div>
   );
 }
